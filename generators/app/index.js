79f27e971133d66315ea0af4c103e5ba596112bb
Joseph Sinfield <josephsinfield@yahoo.com>
2020-03-25 21:30:34 +0000

    Compose genertor-dotfiles in to generator-webpack
    So that we don't have to define the generic dotfiles along with the project specific templates

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,22 +1,23 @@
 const Generator = require('yeoman-generator');
+const DotfilesGenerator = require.resolve('generator-dotfiles/generators/app');
 
 const MyBase = class extends Generator {
   copyTemplateFiles() {
     // Copy all non-dotfiles
     this.fs.copy(
+      // If dotfiles are reintroduced
+      // the repeat this with '.*'
       this.templatePath('**/*'),
-      this.destinationRoot()
-    );
-
-    // Copy all dotfiles
-    this.fs.copy(
-      this.templatePath('.*'),
       this.destinationRoot()
     );
   };
 };
 
 module.exports = class extends MyBase {
+  initializing() {
+    this.composeWith(DotfilesGenerator);
+  };
+
   writing() {
     this.copyTemplateFiles();
     this.config.save();
